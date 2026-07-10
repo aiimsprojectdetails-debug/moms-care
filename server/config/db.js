ca32fdd91c8592847e02c553d268f6375047c148
Amit Khuntia <your-github-email@example.com>
2026-07-10 13:45:16 +0530

    Debug Mongo

--- server/config/db.js
+++ server/config/db.js
@@ -2,17 +2,19 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("✅ MongoDB Connected");
-    console.log(`📂 Database: ${conn.connection.name}`);
-    console.log(`🌐 Host: ${conn.connection.host}`);
-  } catch (error) {
-  console.error("❌ MongoDB Connection Failed");
-  console.error(error);
+    console.log("Connected!");
 
-  throw error;
-}
+    return conn;
+  } catch (err) {
+    console.error("Mongo Error:");
+    console.error(err);
+
+    throw err;
+  }
 };
 
 export default connectDB;
