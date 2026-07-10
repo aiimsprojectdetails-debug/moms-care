459264d1797f28d513db56387280bf400607a9f0
Amit Khuntia <your-github-email@example.com>
2026-07-10 13:40:35 +0530

    Test DB

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import connectDB from "../config/db.js";
+
 dotenv.config();
+
+await connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Backend Working ✅",
-  });
+app.get("/", (req,res)=>{
+    res.json({
+        success:true,
+        message:"Database Connected"
+    });
 });
 
 export default app;
